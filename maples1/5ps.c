3ac53a124239ca07ab195927a25680c66504a1d9
Joe Maples <joe@frap129.org>
2019-10-09 16:39:13 -0400

    5ps: Actually make a C file
    Signed-off-by: Joe Maples <joe@frap129.org>

--- maples1/5ps.c
+++ maples1/5ps.c
@@ -1,1 +1,13 @@
-tmp
+/*
+ * Author: Joseph Maples
+ * Assignment Number: 1
+ * Date of Submission: TBD
+ * Name of this file: 5ps.c
+ * Description of the program: A simple version of ps that accepts 5 arguments
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
